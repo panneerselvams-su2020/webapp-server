a6f2f91cfd891bbf50b7e62e91330ebeb4f38d9e
Sridhar Prasad Panneerselvam <panneerselvam.s@husky.neu.edu>
2020-06-28 10:17:57 -0400

    bug fixes and updates1

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
 # This script is executed before copying the resource
-
-
-export app_root=/home/ubuntu/webapp-server
+export app_root=/home/ubuntu/webapp
 if [ -d "$app_root" ];then
-    rm -rf /home/ubuntu/webapp-server
-    mkdir -p /home/ubuntu/webapp-server
+    rm -rf /home/ubuntu/webapp
+    mkdir -p /home/ubuntu/webapp
 else
-    mkdir -p /home/ubuntu/webapp-server
+    mkdir -p /home/ubuntu/webapp
 fi
