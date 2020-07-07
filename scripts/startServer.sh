14e9f93cf0f749981887b0550efb62efb80b8182
Sridhar Prasad Panneerselvam <panneerselvam.s@husky.neu.edu>
2020-07-07 14:06:46 -0400

    edits in logger2

--- scripts/startServer.sh
+++ scripts/startServer.sh
@@ -2,12 +2,13 @@
 
 # This script is executed after copying the resource
 
-cd /home/ubuntu/webapp/
-java -jar webapp-server-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
-
 #starting cloudwatch agent
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -a fetch-config \
     -m ec2 \
     -c file:/opt/cloudwatch-config.json \
     -s
+
+cd /home/ubuntu/webapp/
+java -jar webapp-server-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+
