bd97b6411e32b3d4af79ddcb6602e4c5c2431847
Sridhar Prasad Panneerselvam <panneerselvam.s@husky.neu.edu>
2020-07-08 11:05:11 -0400

    start server bug fix

--- scripts/startServer.sh
+++ scripts/startServer.sh
@@ -14,7 +14,7 @@ sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -a fetch-config \
     -m ec2 \
-    -c file:/opt/cloudwatch-config.json \
+    -c file:/opt/CloudWatchConfig.json \
     -s
 
 
