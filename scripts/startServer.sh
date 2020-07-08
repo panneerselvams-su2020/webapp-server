fc23dfa8f706ec9c5d7c6fb1462db6f8c8e12fee
Sridhar Prasad Panneerselvam <panneerselvam.s@husky.neu.edu>
2020-07-08 00:02:13 -0400

    start server edit

--- scripts/startServer.sh
+++ scripts/startServer.sh
@@ -5,6 +5,11 @@
 # #copy config file to path
 # sudo cp /home/ubuntu/cloudwatch-config.json /opt/aws/amazon-cloudwatch-agent/etc/
 
+cd /home/ubuntu/webapp/
+java -jar webapp-server-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a stop
+
 #starting cloudwatch agent
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -a fetch-config \
@@ -12,6 +17,5 @@ sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -c file:/opt/cloudwatch-config.json \
     -s
 
-cd /home/ubuntu/webapp/
-java -jar webapp-server-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+
 
