477d46732d5fe1c8f20fb5b6e33166e368306394
Sridhar Prasad Panneerselvam <panneerselvam.s@husky.neu.edu>
2020-07-09 12:38:12 -0400

    cors policy byg fix

--- src/main/java/com/cloud/controller/UserController.java
+++ src/main/java/com/cloud/controller/UserController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -29,7 +30,7 @@ public class UserController {
 	
 	private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-	
+	@CrossOrigin
 	@PostMapping("/signup")
 	public ResponseEntity<User> save(@RequestBody User userObj) {
 		stats.incrementCounter("endpoint.user.addUser.http.post");
